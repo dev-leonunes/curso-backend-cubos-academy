5bbca137ef0572e0c295431cf621366df284e9fe
futuroDevLeo <leonunes07@outlook.com>
2023-10-16 20:49:22 -0300

    refatoração das endpoints de atualizar produto e excluir produto para suportarem query builder (knex)

--- 01-exercicios-backend-query-builder/01/src/controladores/produtos.js
+++ 01-exercicios-backend-query-builder/01/src/controladores/produtos.js
@@ -87,65 +87,28 @@ const atualizarProduto = async (req, res) => {
     const { id } = req.params;
     const { nome, estoque, preco, categoria, descricao, imagem } = req.body;
 
-    if (!nome && !estoque && !preco && !categoria && !descricao && !imagem) {
-        return res.status(404).json('Informe ao menos um campo para atualizaçao do produto');
+    if (!nome || !estoque || !preco || !descricao) {
+        return res.status(404).json('Os campos nome, estoque, preco, descricao são obrigatórios');
     }
 
     try {
-        const query = `select * from produtos where usuario_id = $1 and id = $2`;
-        const { rowCount } = await conexao.query(query, [usuario.id, id]);
+        const produto = await knex('produtos').where({ usuario_id: usuario.id }).andWhere({ id }).first();
 
-        if (rowCount === 0) {
+        if (!produto) {
             return res.status(404).json('Produto não encontrado');
         }
 
-        const body = {};
-        const params = [];
-        let n = 1;
+        const produtoAtualizado = await knex('produtos')
+            .update({
+                nome,
+                estoque,
+                preco,
+                descricao,
+                categoria,
+                imagem
+            }).where({ id })
 
-        if (nome) {
-            body.nome = nome;
-            params.push(`nome = $${n}`);
-            n++;
-        }
-
-        if (estoque) {
-            body.estoque = estoque;
-            params.push(`estoque = $${n}`);
-            n++;
-        }
-
-        if (categoria) {
-            body.categoria = categoria;
-            params.push(`categoria = $${n}`);
-            n++;
-        }
-
-        if (descricao) {
-            body.descricao = descricao;
-            params.push(`descricao = $${n}`);
-            n++;
-        }
-
-        if (preco) {
-            body.preco = preco;
-            params.push(`preco = $${n}`);
-            n++;
-        }
-
-        if (imagem) {
-            body.imagem = imagem;
-            params.push(`imagem = $${n}`);
-            n++;
-        }
-
-        const valores = Object.values(body);
-        valores.push(id);
-        valores.push(usuario.id);
-        const queryAtualizacao = `update produtos set ${params.join(', ')} where id = $${n} and usuario_id = $${n + 1}`;
-        const produtoAtualizado = await conexao.query(queryAtualizacao, valores);
-
-        if (produtoAtualizado.rowCount === 0) {
+        if (produtoAtualizado.length === 0) {
             return res.status(400).json("O produto não foi atualizado");
         }
 
@@ -160,16 +123,15 @@ const excluirProduto = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const query = `select * from produtos where usuario_id = $1 and id = $2`;
-        const { rowCount } = await conexao.query(query, [usuario.id, id]);
+        const produto = await knex('produtos').where({ usuario_id: usuario.id }).andWhere({ id }).first();
 
-        if (rowCount === 0) {
+        if (!produto) {
             return res.status(404).json('Produto não encontrado');
         }
 
-        const produtoExcluido = await conexao.query('delete from produtos where id = $1', [id]);
+        const produtoExcluido = await knex('produtos').del().where({ id }).returning('*');
 
-        if (produtoExcluido.rowCount === 0) {
+        if (produtoExcluido.length === 0) {
             return res.status(400).json("O produto não foi excluido");
         }
 
