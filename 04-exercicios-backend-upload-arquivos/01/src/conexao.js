898af6f3c5a0d36ac5db87d0cb99e504afd7a04d
futuroDevLeo <leonunes07@outlook.com>
2023-10-25 20:02:23 -0300

    feat: instalado as libs multer e aws-sdk para conexão com serviço de armazenamento em nuvem, mudanças de exportação do knex nos controladores de login, produtos e usuarios, adição das credênciais do bucket no .env e criação da conexão com o serviço de armazenamento em nuvem

--- 04-exercicios-backend-upload-arquivos/01/src/conexao.js
+++ 04-exercicios-backend-upload-arquivos/01/src/conexao.js
@@ -8,4 +8,19 @@ const knex = require('knex')({
     }
 });
 
-module.exports = knex;
+const aws = require('aws-sdk');
+
+const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
+
+const s3 = new aws.S3({
+    endpoint,
+    credentials: {
+        accessKeyId: process.env.KEY_ID,
+        secretAccessKey: process.env.KEY_APP
+    }
+});
+
+module.exports = {
+    knex,
+    s3
+};
